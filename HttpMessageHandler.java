f7a221bee306a0a618160542dd4a370573e666cc
Ric Allinson <allinson@yahoo-inc.com>
2016-02-10 22:27:54 -0800

    Fixed keep alive but still not fast enough

--- HttpMessageHandler.java
+++ HttpMessageHandler.java
@@ -17,22 +17,29 @@ class HttpMessageHandler implements Runnable {
     }
 
     public void run() {
-        String LF = "\n";
+        String CRLF = "\r\n";
+        int keepAliveMax = 1000;
         try {
             java.io.InputStream reader = this.socket.getInputStream();
-            reader.read(new byte[1000]);
             java.io.OutputStream writer = this.socket.getOutputStream();
-            String body = "<h1>Hello world</h1>" + LF;
-            writer.write(("HTTP/1.1 200 OK" + LF).getBytes());
-            writer.write(("Content-Length: " + Integer.toString(body.length()) + LF).getBytes());
-            writer.write(("Content-Type: text/html" + LF).getBytes());
-            writer.write(("Connection: close" + LF).getBytes());
-            writer.write(LF.getBytes());
-            writer.write(body.getBytes());
-            writer.flush();
-            writer.close();
+            while (--keepAliveMax >= 0) {
+                reader.read(new byte[1000]);
+                String body = "<h1>Hello world</h1>" + CRLF;
+                writer.write(("HTTP/1.1 200 OK" + CRLF).getBytes());
+                writer.write(("Content-Length: " + Integer.toString(body.length()) + CRLF).getBytes());
+                writer.write(("Content-Type: text/html" + CRLF).getBytes());
+                if (keepAliveMax > 0) {
+                    writer.write(("Keep-Alive: timeout=15, max=" + Integer.toString(keepAliveMax) + CRLF).getBytes());
+                } else {
+                    writer.write(("Connection: close" + CRLF).getBytes());
+                }
+                writer.write(CRLF.getBytes());
+                writer.write(body.getBytes());
+                writer.flush();
+                // System.out.println(Integer.toString(this.id) + ":" + Integer.toString(keepAliveMax));
+            }
+            // writer.close();
             this.socket.close();
-            // System.out.println(this.id);
         } catch (java.io.IOException e) {
             System.out.println(e);
         }
--- README.md
+++ README.md
@@ -17,10 +17,12 @@ This is a simple _hello world_ http server to find a baseline number for perform
 
 Now run apache bench;
 
-	ab -c 20 -n 20000 -k http://127.0.0.1:8080/
+	ab -n 500000 -c 1 -k http://127.0.0.1:8080/
 
-## Problems
+## Problems Found
 
-The server pauses at about 16384 requests. Why? It's too fast.
+The server use to pause at about 16384 requests. Why? It's too fast.
 
-This is because on OSX the default ephemeral port range only allows for 16384 ports. Once they are used up Apache Bench has to wait for them to be freed before it can continue. For more info see this [stackoverflow](http://stackoverflow.com/questions/1216267/ab-program-freezes-after-lots-of-requests-why/1217100#1217100) response.
+This was because on OSX the default ephemeral port range only allows for 16384 ports. Once they are used up Apache Bench has to wait for them to be freed before it can continue. For more info see this [stackoverflow](http://stackoverflow.com/questions/1216267/ab-program-freezes-after-lots-of-requests-why/1217100#1217100) response.
+
+To combat this I've used "keep-alive" so the ports are resued allowing for much greater test runs.
